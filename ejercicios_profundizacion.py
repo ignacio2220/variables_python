c886d83a45efe341526b063c7d1a6ecf39469fe5
ignacio2220 <72768531+ignacio2220@users.noreply.github.com>
2020-10-25 22:09:42 -0300

    Add files via upload

--- ejercicios_profundizacion.py
+++ ejercicios_profundizacion.py
@@ -11,14 +11,30 @@ Programa creado para que practiquen los conocimietos
 adquiridos durante la semana
 '''
 
-__author__ = "Inove Coding School"
-__email__ = "alumnos@inove.com.ar"
+__author__ = "Ignacio Melchiori"
+__email__ = "melchiorignacio@gmail.com"
 __version__ = "1.3"
 
 
 def ej1():
     # Ejercicios de práctica con números
     print('Nuestra primera calculadora')
+    print('Ingrese los números con los que desea operar')
+    numero_1 = float(input())
+    numero_2 = float(input())
+
+    sumar = numero_1 + numero_2
+    restar = numero_1 - numero_2
+    multiplicar = numero_1 * numero_2
+    dividir = numero_1 % numero_2
+    potenciar = numero_1 ** numero_2
+
+    print('La suma entre', numero_1, 'y', numero_2, 'es', sumar)
+    print('La resta entre', numero_1, 'y', numero_2, 'es', restar)
+    print('La multiplicacion entre', numero_1, 'y', numero_2, 'es', multiplicar)
+    print('La division entre', numero_1, 'y', numero_2, 'es', dividir)
+    print( numero_1, 'elevado a', numero_2, 'es', potenciar)
+    
     '''
     Realice un calculadora, se ingresará por línea de comando dos
     números reales y se deberá calcular todas las operaciones entre ellos:
@@ -27,7 +43,7 @@ def ej1():
     - Multiplicación
     - División
     - Exponente/Potencia
-
+  
     - Para todos los casos se debe imprimir en pantalla el resultado aclarando
       la operación realizada en cada caso y con que números
       se ha realizado la operación
@@ -38,13 +54,26 @@ def ej1():
 
 def ej2():
     print('Ejercicios de práctica numérica y cadenas')
+    print("Por favor ingrese su nombre")
+    nombre = str(input())
+    print("Ahora ingrese su apellido")
+    apellido = str(input())
+    nombre_completo = nombre +" " + apellido 
+    print("Ingrese su número de DNI")
+    dni = str(input())
+    print("ingrese su edad")
+    edad = int(input())
+    print("ingrese su altura en cm")
+    altura = int(input())
+
+    print("Su nombre completo es",nombre_completo, "su DNI es", dni, "su edad", edad, "y altura", altura, "cm")
     '''
     Realice un programa que consulte por consola:
     - El nombre completo de la persona
     - El DNI de la persona
     - La edad de la persona
     - La altura de la persona
-
+  
     Finalmente el programa debe imprimir dos líneas de texto por separado
     - En una línea imprimir el nombre completo y el DNI, aclarando de que
       campo se trata cada uno
@@ -59,6 +88,18 @@ def ej2():
 
 def ej3():
     print('Ejercicios de práctica con cadenas')
+    print("por favor ingrese el nombre completo de su padre")
+    padre_1 = str(input())
+    print("por favor ingrese el nombre completo de sus madre")
+    padre_2 = str(input())
+    
+    nombre_padre_1, apellido_padre_1 = padre_1.split(' ')
+    nombre_padre_2, apellido_padre_2 = padre_2.split(' ')
+
+    print("por favor ingrese su nombre")
+    hijo = str(input())
+    print("Su nombre es", hijo, apellido_padre_1, apellido_padre_2)
+
 
     '''
     Realice un programa que determine cual sería el apellido de una persona
@@ -94,6 +135,16 @@ def ej3():
 def ej4():
     # Ejercicios de práctica con cadenas
     print('Comencemos a ponernos serios')
+    print("ingrese el nombre y apellid de una persona")
+    persona_1 = str(input())
+    
+    print("ingrese el nombre y apellid de una persona")
+    persona_2 = str(input())
+    nombre_persona_2, apellido_persona_2 = persona_2.split(" ")
+    
+    son_parientes = apellido_persona_2 in persona_1
+    print(persona_2, 'es pariente de', persona_1,'?', son_parientes)
+
     '''
     Realice un programa que determine si una persona_2
     es pariente de la persona_1
@@ -128,6 +179,14 @@ def ej4():
 def ej5():
     # Ejercicios de práctica con cadenas
     print('Ahora si! buena suerte!')
+    print("Por favor ingrese su nombre completo")
+    nombre_compelto = str(input())
+
+    print('Su nombre en minusculas es:',nombre_compelto.lower())
+    print('Su nombre en mayusculas es:',nombre_compelto.upper())
+    nombre , apellido = nombre_compelto.split(" ")
+    print(nombre.capitalize(), apellido.capitalize())
+
     '''
     Realice un programa que reciba por consola su nombre completo
     e imprima en pantalla su nombre en los siguientes formatos:
@@ -154,7 +213,7 @@ def ej5():
 if __name__ == '__main__':
     print("Ejercicios de práctica")
     ej1()
-    # ej2()
-    # ej3()
-    # ej4()
-    # ej5()
+    ej2()
+    ej3()
+    ej4()
+    ej5()
